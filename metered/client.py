9bd4e27bfb397a50fb9c85baa6dd2ffb1bc29e29
Adam Bouhenguel <ajbouh@users.noreply.github.com>
2023-04-21 22:51:49 -0700

    more

--- metered/client.py
+++ metered/client.py
@@ -6,26 +6,27 @@ import json
 from functools import partial as _partial
 from typing import Any
 
-def request(host: str, query: str, variables: Dict[Any]={}, path: str = "/"):
-  payload = {
-    "query": query,
-    "variables": variables,
-  }
+class GraphQLClient:
+  def __init__(self, host: str, path: str = "/") -> None:
+    self._host = host
+    self._path = path
 
-  conn = http.client.HTTPSConnection(host)
-  conn.request("POST", path, json.dumps(payload), {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer " + os.environ['METERED_API_KEY']
-  })
-  res = conn.getresponse()
-  data = res.read()
-  json_data = json.loads(data.decode("utf-8"))
+  def request(self, query: str, variables: Dict[Any]={}):
+    payload = {
+      "query": query,
+      "variables": variables,
+    }
 
-  if "errors" in json_data and json_data["errors"]:
-    raise Exception(json.dumps(json_data["errors"]))
-  
-  return json_data
+    conn = http.client.HTTPSConnection(self._host)
+    conn.request("POST", self._path, json.dumps(payload), {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + os.environ['METERED_API_KEY']
+    })
+    res = conn.getresponse()
+    data = res.read()
+    json_data = json.loads(data.decode("utf-8"))
 
-def partial(**kw):
-  global request
-  return _partial(request, **kw)
+    if "errors" in json_data and json_data["errors"]:
+      raise Exception(json.dumps(json_data["errors"]))
+    
+    return json_data
