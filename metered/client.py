3251e7d6d4bcf99e0acaac5e34d2925fdb3ef19a
Adam Bouhenguel <ajbouh@users.noreply.github.com>
2023-04-21 22:47:49 -0700

    try

--- metered/client.py
+++ metered/client.py
@@ -7,24 +7,25 @@ from functools import partial as _partial
 from typing import Any
 
 def request(host: str, query: str, variables: Dict[Any]={}, path: str = "/"):
-    payload = {
-      "query": query,
-      "variables": variables,
-    }
+  payload = {
+    "query": query,
+    "variables": variables,
+  }
 
-    conn = http.client.HTTPSConnection(host)
-    conn.request("POST", path, json.dumps(payload), {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer " + os.environ['METERED_API_KEY']
-    })
-    res = conn.getresponse()
-    data = res.read()
-    json_data = json.loads(data.decode("utf-8"))
+  conn = http.client.HTTPSConnection(host)
+  conn.request("POST", path, json.dumps(payload), {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + os.environ['METERED_API_KEY']
+  })
+  res = conn.getresponse()
+  data = res.read()
+  json_data = json.loads(data.decode("utf-8"))
 
-    if "errors" in json_data and json_data["errors"]:
-      raise Exception(json.dumps(json_data["errors"]))
-    
-    return json_data
+  if "errors" in json_data and json_data["errors"]:
+    raise Exception(json.dumps(json_data["errors"]))
+  
+  return json_data
 
 def partial(**kw):
-    return _partial(request, **kw)
+  global request
+  return _partial(request, **kw)
